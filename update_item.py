d4c3d37161aeb727e1b197f1ffd60ce3438f2736
Alejandra Quetzalli <alejandra.olvera.novack@gmail.com>
2020-02-05 13:49:02 -0800

    fixing wrong Schema

--- update_item.py
+++ update_item.py
@@ -7,14 +7,14 @@ dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 table = dynamodb.Table('Bandanas')
 
 # We'll use the GetItem API to show what the book looks like before we update it.
-resp = table.get_item(Key={"Color": "hot pink", "Material": "cotton"})
+resp = table.get_item(Key={"Material": "cotton", "Shade": "pastel::baby blue"})
 
 print("Item before update:")
 print(resp['Item'])
 
 # The UpdateItem API allows us to update a particular item as identified by its key.
 resp = table.update_item(
-    Key={"Color": "hot pink", "Material": "cotton"},
+    Key={"Material": "cotton", "Shade": "pastel::baby blue"},
     # Expression attribute names specify placeholders for attribute names to use in your update expressions.
     ExpressionAttributeNames={
         "#formats": "Formats",
@@ -22,7 +22,7 @@ resp = table.update_item(
     },
     # Expression attribute values specify placeholders for attribute values to use in your update expressions.
     ExpressionAttributeValues={
-        ":id": "B07VWV4M3R",
+        ":id": "B02VWV4M3T",
     },
     # UpdateExpression declares the updates we want to perform on our item.
     # For more details on update expressions, see https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.UpdateExpressions.html
@@ -31,7 +31,7 @@ resp = table.update_item(
 
 
 # Now use the GetItem API to show what the book looks like after the update.
-resp = table.get_item(Key={"Color": "hot pink", "Material": "cotton"})
+resp = table.get_item(Key={"Material": "cotton", "Shade": "pastel::baby blue"})
 
 print("Item after update:")
 print(resp['Item'])
