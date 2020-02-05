b9c56eddd670673412b5d6d58890e0642820a7aa
Alejandra Quetzalli <alejandra.olvera.novack@gmail.com>
2020-02-05 13:47:32 -0800

    fixing wrong Schema

--- insert_items.py
+++ insert_items.py
@@ -10,49 +10,46 @@ table = dynamodb.Table('Bandanas')
 # The BatchWriteItem API allows us to write multiple items to a table in one request.
 with table.batch_writer() as batch:
     batch.put_item(Item={"Color": "hot pink", "Material": "cotton",
-        "Shade": "dark", "Formats": { "ISBN": "B07VWV4M3R"} })
+        "Shade": "dark::hot pink", "Formats": { "ISBN": "B07VWV4M3R"} })
 
     batch.put_item(Item={"Color": "red", "Material": "cotton",
-        "Shade": "dark", "Formats": { "ISBN": "B07VWV4M4R"} })
+        "Shade": "dark::red", "Formats": { "ISBN": "B07VWV4M4R"} })
 
     batch.put_item(Item={"Color": "black", "Material": "cotton",
-        "Shade": "dark", "Formats": { "ISBN": "B07VWV4M5R"} })
+        "Shade": "dark::black", "Formats": { "ISBN": "B07VWV4M5R"} })
 
     batch.put_item(Item={"Color": "soft pink", "Material": "cotton",
-        "Shade": "pastel", "Formats": { "ISBN": "B07VWV4M6R"} })
+        "Shade": "pastel::soft pink", "Formats": { "ISBN": "B07VWV4M6R"} })
 
     batch.put_item(Item={"Color": "lilac", "Material": "cotton",
-        "Shade": "partel", "Formats": { "ISBN": "B07VWV4M7R"} })
-    
-    batch.put_item(Item={"Color": "black", "Material": "cotton",
-        "Shade": "dark", "Formats": { "ISBN": "B07VWV4M8R"} })    
+        "Shade": "partel::lilac", "Formats": { "ISBN": "B07VWV4M7R"} })
     
     batch.put_item(Item={"Color": "navy blue", "Material": "cotton",
-        "Shade": "dark", "Formats": { "ISBN": "B07VWV4M9R"} })
+        "Shade": "dark::navy blue", "Formats": { "ISBN": "B07VWV4M9R"} })
 
     batch.put_item(Item={"Color": "light grey", "Material": "velvet",
-        "Shade": "pastel", "Formats": { "ISBN": "B01VWV4M3R"} })
+        "Shade": "pastel::light grey", "Formats": { "ISBN": "B01VWV4M3R"} })
 
     batch.put_item(Item={"Color": "baby blue", "Material": "cotton",
-        "Shade": "pastel", "Formats": { "ISBN": "B02VWV4M3R"} })
+        "Shade": "pastel::baby blue", "Formats": { "ISBN": "B02VWV4M3R"} })
 
     batch.put_item(Item={"Color": "orange", "Material": "velvet",
-        "Shade": "dark", "Formats": { "ISBN": "B03VWV4M3R"} })  
+        "Shade": "dark::orange", "Formats": { "ISBN": "B03VWV4M3R"} })  
 
     batch.put_item(Item={"Color": "dark purple", "Material": "cotton",
-        "Shade": "dark", "Formats": { "ISBN": "B04VWV4M3R"} })
+        "Shade": "dark::dark purple", "Formats": { "ISBN": "B04VWV4M3R"} })
 
     batch.put_item(Item={"Color": "kelly green", "Material": "cotton",
-        "Shade": "dark", "Formats": { "ISBN": "B05VWV4M3R"} })
+        "Shade": "dark::kelly green", "Formats": { "ISBN": "B05VWV4M3R"} })
 
     batch.put_item(Item={"Color": "yellow", "Material": "velvet",
-        "Shade": "dark", "Formats": { "ISBN": "B06VWV4M3R"} })
+        "Shade": "dark::yellow", "Formats": { "ISBN": "B06VWV4M3R"} })
 
     batch.put_item(Item={"Color": "brown", "Material": "cotton",
-        "Shade": "dark", "Formats": { "ISBN": "B08VWV4M3R"} })
+        "Shade": "dark::brown", "Formats": { "ISBN": "B08VWV4M3R"} })
 
     batch.put_item(Item={"Color": "light yellow", "Material": "velvet",
-        "Shade": "pastel", "Formats": { "ISBN": "B09VWV4M3R"} })
+        "Shade": "pastel::light yellow", "Formats": { "ISBN": "B09VWV4M3R"} })
 
     batch.put_item(Item={"Color": "white", "Material": "cotton",
-        "Shade": "pastel", "Formats": { "ISBN": "B07VWV5M3R"} })
+        "Shade": "pastel::white", "Formats": { "ISBN": "B07VWV5M3R"} })
